9f57e317d54ff357976c69f7b122fe30081fe95b
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-28 15:40:17 -0400

    added style.css and script.js links as well as bootstrap stylesheets and JS links

--- assets/js/wordbank.js
+++ assets/js/wordbank.js
@@ -1,5 +1,21 @@
 // function to get local storage and present stored word buttons
+let wordBankEl = document.getElementById('word-bank');
+
+let wordArray = ['Buffy', 'Willow', 'Tara', 'Zander', 'Giles', 'Anya', 'Spike', 'Angel']
+
+function showWords(array){
+    for(let i=0; i<wordArray.length; i++) {
+        let wordButtons = document.createElement('button');
+        wordButtons.textContent =wordArray[i];
+        console.log(wordButtons);
+        wordBankEl.append(wordButtons);
+    }
+}
+
+showWords(wordArray);
+
 // load stats bar (possibly tailwind)
 // event listener on each word to get correct info possibly from API fetch - get same word details as at end of each round
 // button to go back to home page
 // function delete words 
+
