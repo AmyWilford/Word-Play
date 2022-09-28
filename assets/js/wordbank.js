0b824b88db5d0ca23aba892075671df6cb3dffc8
saharsh <saharshdc@gmail.com>
2022-09-28 13:06:32 -0400

    added Pseudocode to both JS files and wordbank html file

---
+++ assets/js/wordbank.js
@@ -0,0 +1,5 @@
+// function to get local storage and present stored word buttons
+// load stats bar (possibly tailwind)
+// event listener on each word to get correct info possibly from API fetch - get same word details as at end of each round
+// button to go back to home page
+// function delete words 
