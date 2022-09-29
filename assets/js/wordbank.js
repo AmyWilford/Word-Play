eed3dd8725b1074a065b2fd53f165465096ffe64
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-28 20:23:34 -0400

    styled button footer on homepage index.html

--- assets/js/wordbank.js
+++ assets/js/wordbank.js
@@ -2,7 +2,7 @@
 let wordBankEl = document.getElementById('word-bank');
 let homeButtonEl = document.getElementById('home-button')
 
-let wordArray = ['Buffy', 'Willow', 'Tara', 'Zander', 'Giles', 'Anya', 'Spike', 'Angel', ['Drucilla']]
+let wordArray = ['Buffy', 'Willow', 'Tara', 'Zander', 'Giles', 'Anya', 'Spike', 'Angel', 'Drucilla'];
 
 // Function to push buttons to page
 function showWords(array){
@@ -23,6 +23,7 @@ showWords(wordArray);
 homeButtonEl.addEventListener('click', goHome);
 
 // event listener on each word to get correct info possibly from API fetch - get same word details as at end of each round
+
 // function delete words 
 
 // load stats bar (possibly tailwind)
