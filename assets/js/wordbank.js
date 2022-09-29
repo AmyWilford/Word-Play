0aa965c4a053541c4be10b6756fb57dc1fa4510a
Amy Wilford <amywilford@Amys-MacBook-Air.local>
2022-09-28 21:01:17 -0400

    added event listener to each word button that can pull index name

--- assets/js/wordbank.js
+++ assets/js/wordbank.js
@@ -1,18 +1,26 @@
 // function to get local storage and present stored word buttons
 let wordBankEl = document.getElementById('word-bank');
 let homeButtonEl = document.getElementById('home-button')
+let modalEl = document.getElementById('wordModal');
 
 let wordArray = ['Buffy', 'Willow', 'Tara', 'Zander', 'Giles', 'Anya', 'Spike', 'Angel', 'Drucilla'];
+let wordButton;
 
 // Function to push buttons to page
 function showWords(array){
     for(let i=0; i<wordArray.length; i++) {
-        let wordButton = document.createElement('button');
+        wordButton = document.createElement('button');
         wordButton.classList.add('custom-button');
         wordButton.textContent =wordArray[i];
         wordBankEl.append(wordButton);
-    };
+        // add event listener to each button
+        // Pull API details into modal
+        wordButton.addEventListener('click', function() {
+            console.log(wordArray[i] + ' clicked');
+        })
+    }
 }
+    
 
 // function to go back to home page
 function goHome() {
@@ -21,6 +29,8 @@ function goHome() {
 
 showWords(wordArray);
 homeButtonEl.addEventListener('click', goHome);
+
+// wordButton.addEventListener('click', showModal);
 
 // event listener on each word to get correct info possibly from API fetch - get same word details as at end of each round
 
